955856fd19c23aa76d5f2a9213f5d95e03461eed
mpj144 <marcelopjrdev@gmail.com>
2021-05-21 20:15:18 -0300

    algumas att

--- js/function.js
+++ js/function.js
@@ -7,6 +7,7 @@ function getTamanhoCamisa(inputV) {
     console.log(InputValue)
     if (InputValue == "P" || InputValue == "M" || InputValue == "G") {
         document.getElementById("ButaoAddCarrinho").disabled=false;
+        document.getElementById("aviso").innerHTML = "";
     }
     
 }
@@ -16,4 +17,26 @@ function verifyCompra() {
         alert("Produto Adicionado")
     
 }
+
+
+
+$(document).ready(function (e) {
+    
+    $("#EscolhaNumeroS").click(function (e) {
+        $("#divEscolhaNum").show();
+        
+    });
+    $("#EscolhaNumeroN").click(function (e) {
+        $("#divEscolhaNum").hide();
+    });
+
+
+    $("#EscolhaNomeS").click(function (e) {
+        $("#divEscolhaNome").show();
+    });
+    $("#EscolhaNomeN").click(function (e) {
+        $("#divEscolhaNome").hide();
+    });
+});
+
 
